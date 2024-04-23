75f475a286c3839401949fd4e990de75209bd97d
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-04-23 17:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,26 +10,20 @@ const images = [
 // Function to download an image and return a promise
 function downloadImage(image) {
   return new Promise((resolve, reject) => {
-    const imgElement = new Image();
+    const imgElement = new Image(); 
     imgElement.onload = () => resolve(imgElement);
     imgElement.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
     imgElement.src = image.url;
   });
 }
 
-// Function to download all images in parallel and display them
-async function downloadAndDisplayImages(images) {
-  try {
-    const downloadedImages = await Promise.all(images.map(downloadImage));
-    downloadedImages.forEach(imgElement => {
-      output.appendChild(imgElement);
-    });
-  } catch (error) {
-    console.error(error.message);
-  }
-}
+
 
 // Event listener for the button click
 btn.addEventListener("click", () => {
-  downloadAndDisplayImages(images);
+  Promise.all(images.map(downloadImage)).then((imgs)=>{
+	 imgs.forEach((a)=>{
+		 output.append(a);
+	 }); 
+  }).catch((err)=>console.log(err.message));
 });
